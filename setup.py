9164d650a302e4a715d3ec2aa3e8cb697a278c94
Jon Staff <jstaff@indatus.com>
2014-06-11 13:16:49 -0400

    Full rename to Paladin

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ with open('description.rst', 'rb') as f:
 	long_descr = f.read().decode('utf-8')
 
 setup(
-	name = 'r2d2',
-	packages = ['r2d2'],
+	name = 'paladin',
+	packages = ['paladin'],
 	entry_points = {
-		'console_scripts': ['r2d2 = r2d2.r2d2:main']
+		'console_scripts': ['paladin = paladin.paladin:main']
 	},
 	version	= '0.3.7',
-	description = "Android Dependency Manager for libraries that aren't packaged as .jar or .aar",
+	description = "Android Dependency Manager for libraries that aren't packaged as .jar or .aar - Paladin fells your dependencies like a boss.",
 	long_description = long_descr,
 	author = "Jonathon Staff",
 	author_email = "jon@nplexity.com",
-	url = "http://jonathonstaff.com/r2d2"
+	url = "http://jonathonstaff.com/paladin"
 	)
